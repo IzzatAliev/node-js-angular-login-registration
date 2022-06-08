c3bd0559ec1cf62b56c28ad456490700add98137
IzzatAliev <alibax1515@gmail.com>
2022-06-08 21:14:19 +0200

    fix form

--- server/server.js
+++ server/server.js
@@ -3,10 +3,22 @@ const cors = require("cors");
 const cookieSession = require("cookie-session");
 const db = require("./app/models/index");
 const app = express();
+const i18 = require('i18n-iso-countries')
 
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and Resync Db");
+  init();
 });
+
+function init(){
+    const countries = i18.getSupportedLanguages()
+    for (let i = 0; i < countries.length; i++) {
+            if(i18.getName(countries[i].toUpperCase(),"en") !== undefined){
+                db.country.create({ name: i18.getName(countries[i], 'en')})
+            }
+    }
+    db.country.create({ name: 'Ukraine' })
+}
 
 app.use(
   cors({
